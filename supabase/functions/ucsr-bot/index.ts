f2dc34325745ca68d5e7202c3c1c3e4dd55b525e
rezyaev <danil.rezyaev@gmail.com>
2023-03-02 00:54:50 +0300

    use deno-postgres instead of supabase client

--- supabase/functions/ucsr-bot/index.ts
+++ supabase/functions/ucsr-bot/index.ts
@@ -1,30 +1,50 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
-import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.14.1/mod.ts";
 import { format } from "https://deno.land/std@0.177.0/datetime/mod.ts";
-import { Database } from "./database.types.ts";
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.14.1/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-console.log("UCSR Bot is running...");
-
-const supabase = createClient<Database>("https://tbympouurznyzuqonorf.supabase.co", Deno.env.get("SUPABASE_KEY")!);
 const bot = new Bot(Deno.env.get("BOT_TOKEN")!);
-
 await bot.api.setMyCommands([
 	{ command: "showfantasywins", description: "Показать кто сколько раз выиграл в fantasy" },
 	{ command: "addfantasywin", description: "Добавить победу в fantasy" },
 	{ command: "showtournaments", description: "Показать доступные турниры" },
 ]);
 
-bot.command("showfantasywins", async (ctx) => {
-	const { data: members, error: membersError } = await supabase.from("Members").select("*");
-	if (!members || membersError) {
-		return ctx.reply(`Не удалось загрузить игроков`);
-	}
+const client = new Client(Deno.env.get("DATABASE_URL")!);
+const handleUpdate = webhookCallback(bot, "std/http");
 
-	const { data: tournaments, error: tournamentsError } = await supabase.from("Tournaments").select("*");
-	if (!tournaments || tournamentsError) {
-		return ctx.reply(`Не удалось загрузить турниры`);
+serve(async (req) => {
+	try {
+		if (new URL(req.url).searchParams.get("secret") !== bot.token) {
+			return new Response("not allowed", { status: 405 });
+		}
+
+		await client.connect();
+		return await handleUpdate(req);
+	} catch (err) {
+		console.error(err);
+		await client.end();
 	}
+});
+
+type Member = {
+	id: number;
+	telegram_username: string;
+	steam_username: string;
+};
+
+type Tournament = {
+	id: number;
+	name: string;
+	start: string;
+	finish: string;
+	fantasy_winner: number;
+};
+
+bot.command("showfantasywins", async (ctx) => {
+	const { rows: members } = await client.queryObject<Member>`SELECT * FROM members`;
+	const { rows: tournaments } = await client.queryObject<Tournament>`SELECT * FROM tournaments`;
 
 	return ctx.reply(
 		members
@@ -45,48 +65,25 @@ bot.command("addfantasywin", async (ctx) => {
 	}
 	const [_, telegramUsername, tournamentName] = match;
 
-	const { data: members, error: membersError } = await supabase
-		.from("Members")
-		.select("id, telegram_username")
-		.eq("telegram_username", telegramUsername);
-	if (!members || membersError) {
-		return ctx.reply(`Не удалось найти @${telegramUsername}`);
-	}
-	const winner = members[0];
+	const winner = (await client.queryObject<Member>`SELECT * FROM members WHERE telegram_username = ${telegramUsername}`)
+		.rows[0];
+	const wonTournament = (await client.queryObject<Tournament>`SELECT * FROM tournaments WHERE name = ${tournamentName}`)
+		.rows[0];
 
-	const { data: tournaments, error: tournamentsError } = await supabase
-		.from("Tournaments")
-		.select("*")
-		.eq("name", tournamentName);
-	if (!tournaments || tournaments.length !== 1 || tournamentsError) {
-		return ctx.reply(`Не удалось найти ${tournamentName}`);
-	}
-	const tournament = tournaments[0];
-
-	if (tournament.fantasy_winner !== null) {
-		return ctx.reply(`У <b>${tournament.name}</b> уже есть победитель!`, { parse_mode: "HTML" });
+	if (wonTournament.fantasy_winner !== null) {
+		return ctx.reply(`У <b>${wonTournament.name}</b> уже есть победитель!`, { parse_mode: "HTML" });
 	}
 
-	const { data: updatedTournaments, error: updatedTournamentsError } = await supabase
-		.from("Tournaments")
-		.update({ fantasy_winner: winner.id })
-		.eq("name", tournamentName)
-		.select();
-	if (!updatedTournaments || updatedTournaments.length !== 1 || updatedTournamentsError) {
-		return ctx.reply(`Не удалось обновить ${tournamentName}`);
-	}
+	await client.queryObject<Tournament>`UPDATE tournaments SET fantasy_winner = ${winner.id} WHERE name = ${tournamentName}`;
 
 	return ctx.reply(
-		`Поздравляю, @${winner.telegram_username}! Ты разъебал нубасиков и выиграл <b>${tournament.name} Fantasy</b>!`,
+		`Поздравляю, @${winner.telegram_username}! Ты разъебал нубасиков и выиграл <b>${wonTournament.name} Fantasy</b>!`,
 		{ parse_mode: "HTML" }
 	);
 });
 
 bot.command("showtournaments", async (ctx) => {
-	const { data: tournaments, error } = await supabase.from("Tournaments").select("*");
-	if (!tournaments || error) {
-		return ctx.reply("Не удалось загрузить турниры");
-	}
+	const { rows: tournaments } = await client.queryObject<Tournament>`SELECT * FROM tournaments`;
 
 	return ctx.reply(
 		tournaments
@@ -98,20 +95,4 @@ bot.command("showtournaments", async (ctx) => {
 			.join("\n"),
 		{ parse_mode: "HTML" }
 	);
-});
-
-const handleUpdate = webhookCallback(bot, "std/http");
-serve(async (req) => {
-	try {
-		console.log("Got request", req);
-
-		const url = new URL(req.url);
-		if (url.searchParams.get("secret") !== bot.token) {
-			return new Response("not allowed", { status: 405 });
-		}
-
-		return await handleUpdate(req);
-	} catch (err) {
-		console.error(err);
-	}
 });
